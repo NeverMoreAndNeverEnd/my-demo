874ea98fc94d11ae7ab9c262d3166cbd7e5672a2
闵继弘 <15995283349@163.com>
2020-02-06 23:49:06 +0800

    GC demo

---
+++ nettyclient/src/main/java/com/soul/jvm/HelloGC.java
@@ -0,0 +1,11 @@
+package com.soul.jvm;
+
+public class HelloGC {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("**************hello gc");
+        Thread.sleep(Integer.MAX_VALUE);
+    }
+
+
+}
